fac703019f781da318d4c5ed0a86ad150ebe8733
Tatoline <taylan@yield.app>
2023-09-15 16:23:39 +0300

    Logo Component and Image Added

---
+++ src/components/Logo.js
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types'
+
+const Logo = ({title, logo}) => {
+    return(
+        <div className="flex items-center">
+            <img src={logo} alt={title} className="mr-3 h-6" />
+            <span className="self-center text-xl font-semibold whitespace-nowrap text-white">
+                {title}
+            </span>
+        </div>
+    )
+}
+
+Logo.propTypes = {
+    title: PropTypes.string,
+    logo: PropTypes.string
+}
+
+Logo.defaultProps = {
+    title: 'Covid-19 Tracking App',
+    logo: 'https://github.com/tatoline/covid-19_tracking_app/tree/master/src/img/logo.png'
+}
+
+export default Logo
