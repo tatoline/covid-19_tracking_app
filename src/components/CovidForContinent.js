4a12910a325bf31008de4b6b7ee93ba61130a159
Tatoline <taylan@yield.app>
2023-10-02 12:26:47 +0300

    Continent Statistics Added (non-responsive)

--- src/components/CovidForContinent.js
+++ src/components/CovidForContinent.js
@@ -1,12 +1,17 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
 import { API_TOKEN, API_URL } from "../const"
-import Continents from "../const/Continents"
+import Continents from "./Continents"
+import ContinentBox from "./ContinentBox"
 
 const CovidForContinent = () => {
 
     const [isLoading, setLoading] = useState(false)
     const [data, setData] =useState([])
+    const [activeContinent, setActiveContinent] = useState()
+    const [isHovered, setHovered] = useState(false)
+    const [timerId, setTimerId] = useState(null)  // When a continent unhovered, info about that continent still will be visible for a specific second
+    const [hide, setHide] = useState(true)
 
     const fetchData = async () => {
         setLoading(true)
@@ -23,12 +28,39 @@ const CovidForContinent = () => {
         fetchData()
     }, [])
 
+    useEffect(() => {
+
+        /* This is blocking the flashing of continent info box.
+        *  Continent SVG image has white gaps between countries.
+        *  Since end-user without noticing moving their cursor from gaps,
+        *  a little timeout is enough for starting hide process.
+        */
+        if (!isHovered) {
+            const newTimerId = setTimeout(() => {
+                setHide(true)
+            }, 500)
+
+            setTimerId(newTimerId)
+            }
+
+            else {
+                setHide(false)
+                if (timerId) {
+                    clearTimeout(timerId)
+                }
+            }
+    }, [isHovered])
+
     return(
-        <div className="flex items-center justify-center">
-            <Continents />
+        <div className="flex items-center justify-center -tr">
+            {isLoading ? 'Loading...' :
+            <>
+                <Continents isHovered={(bool) => setHovered(bool)} activeContinent={(id) => setActiveContinent(id)} />
+                <ContinentBox activeContinent={activeContinent} hide={hide} data={data} />
+            </>
+            }
         </div>
     )
-
 }
 
 export default CovidForContinent
