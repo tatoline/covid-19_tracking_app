2b95ccc4754c6ea4d2bcccbc6d0a0a846e6461d2
Tatoline <taylan@yield.app>
2023-10-16 17:09:29 +0300

    Version number added to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,14 @@
 const Footer = () => {
+    const packageJSON = require('./../../package.json')
+
     return(
-        <footer className="bg-gray-800 border-gray-200 px-2 py-2.5 fixed bottom-0 left-0 w-full">
+        <footer className="bg-gray-800 border-gray-200 px-2 pt-2.5 pb-0 fixed bottom-0 left-0 w-full">
             <div className="container flex flex-wrap justify-evenly items-center mx-auto text-white">
                 <span className="flex flex-row">Coded by&nbsp;<a href="https://github.com/tatoline" className="text-blue-700">Tatoline</a></span>
                 <span className="flex flex-row">Open Source React Project on&nbsp;<a href="https://github.com/tatoline/covid-19_tracking_app" className="text-blue-700">GitHub</a></span>
+            </div>
+            <div className="container flex flex-wrap justify-evenly items-center mx-auto text-gray-600 text-xs">
+                v{packageJSON.version}
             </div>
         </footer>
     )
